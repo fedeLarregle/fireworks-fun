59ee52342d911d4eb48c2a335b8cc665a65ee82d
DESKTOP-VMSGSAC\fedel <fede.larregle@gmail.com>
2018-08-12 22:42:14 -0300

    Adding main body to App class.

--- src/com/larregle/App.java
+++ src/com/larregle/App.java
@@ -1,5 +1,23 @@
 package com.larregle;
 
+import com.larregle.fireworks.Fireworks;
+import com.larregle.fireworks.FireworksCanvas;
+
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
 public class App {
-    public static void main(String... args) {}
+    public static void main(String... args) {
+        Fireworks fireworks = new Fireworks();
+        FireworksCanvas fireworksCanvas = new FireworksCanvas(fireworks);
+        JFrame frame = new JFrame();
+        frame.setTitle("Fireworks");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.getContentPane().add(fireworksCanvas);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        fireworksCanvas.requestFocus();
+        fireworksCanvas.start();
+    }
 }
